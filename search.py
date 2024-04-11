aaef7be6c770872727ff24b1c8652cd14f9943d9
rzfzr <capitao.rafa@gmail.com>
2024-04-11 18:24:16 -0300

    Fixed getting jobs page url

--- search.py
+++ search.py
@@ -23,14 +23,21 @@ date_stamp = int(time.mktime(datetime.now().timetuple()))
 html_bookmarks += create_subfolder("Extracted Jobs", date_stamp)
 
 
-
 with open('README.md', 'r', encoding='utf-8', errors='ignore') as md_file:
     for line in md_file:
         parts = line.split(' | ')
         if len(parts) >= 3 and any(term in parts[2].lower() for term in ['br', 'latam', 'americas', 'worldwide']):
-            bookmark_name = parts[0].strip('[]').split(']')[0]
-            bookmark_link = parts[0].split('](')[-1].strip(')')
-            html_bookmarks += f'    <DT><A HREF="{bookmark_link}" ADD_DATE="">{bookmark_name}</A>\n'
+
+            match = re.search(r'\[([^\]]+)\]\(([^)]+)\)', parts[0])
+            if match:
+                filename = match.group(2).strip()[1:]
+                markdown_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+                with open(markdown_file_path, 'r', encoding='utf-8') as file_to_read:
+                            content = file_to_read.read()
+                            last_url = extract_last_url(content)
+                            if last_url:
+                                bookmark_name = parts[0].strip('[]').split(']')[0]
+                                html_bookmarks += f'        <DT><A HREF="{last_url}" ADD_DATE="{date_stamp}">{bookmark_name}</A>\n'
 
 
 
