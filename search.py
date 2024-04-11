0cb5b000acd8ed01e3ca2058749c62066c770836
rzfzr <capitao.rafa@gmail.com>
2024-04-11 18:08:33 -0300

    Generating bookmarks

--- search.py
+++ search.py
@@ -1,5 +1,7 @@
 import os
 import re
+import time
+from datetime import datetime
 
 def extract_last_url(md_content):
     urls = re.findall(r'\(https?://[^\s)]+\)', md_content)
@@ -7,19 +9,32 @@ def extract_last_url(md_content):
         return urls[-1].strip('()')
     return None
 
-with open('results_file.txt', 'w') as results_file:
-    with open('README.md', 'r',encoding='utf-8') as md_file:
-        lines = md_file.readlines()
+def create_subfolder(folder_name, date_stamp):
+    return f'    <DT><H3 ADD_DATE="{date_stamp}" LAST_MODIFIED="{date_stamp}">{folder_name}</H3>\n    <DL><p>\n'
 
-    for line in lines:
-        parts = line.split('|')
-        if len(parts) > 2 and any(keyword in parts[2].lower() for keyword in [' br', 'latam', 'americas', 'worldwide']):
-            match = re.search(r'\[([^\]]+)\]\(([^)]+)\)', parts[0])
-            if match:
-                filename = match.group(2).strip()[1:]
-                markdown_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
-                with open(markdown_file_path, 'r',encoding='utf-8') as file_to_read:
-                    content = file_to_read.read()
-                    last_url = extract_last_url(content)
-                    if last_url:
-                        results_file.write(last_url + '\n')
+html_bookmarks = '''
+<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks</H1>
+<DL><p>
+'''
+date_stamp = int(time.mktime(datetime.now().timetuple()))
+html_bookmarks += create_subfolder("Extracted Jobs", date_stamp)
+
+
+
+with open('README.md', 'r', encoding='utf-8', errors='ignore') as md_file:
+    for line in md_file:
+        parts = line.split(' | ')
+        if len(parts) >= 3 and any(term in parts[2].lower() for term in ['br', 'latam', 'americas', 'worldwide']):
+            bookmark_name = parts[0].strip('[]').split(']')[0]
+            bookmark_link = parts[0].split('](')[-1].strip(')')
+            html_bookmarks += f'    <DT><A HREF="{bookmark_link}" ADD_DATE="">{bookmark_name}</A>\n'
+
+
+
+html_bookmarks += '    </DL><p>\n</DL><p>'
+
+with open('bookmarks.html', 'w', encoding='utf-8') as html_file:
+    html_file.write(html_bookmarks)
